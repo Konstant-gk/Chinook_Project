f1fcbb49bd84c84fafd976fbf39810e19765aab2
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-14 18:01:39 +0200

    Update Chinook_DW_Script_Load_Data.sql

--- Chinook_Data_Warehouse_Files/Chinook_DW_Solution/SQL Server Scripts/Chinook_DW_Script_Load_Data.sql
+++ Chinook_Data_Warehouse_Files/Chinook_DW_Solution/SQL Server Scripts/Chinook_DW_Script_Load_Data.sql
@@ -1,74 +1,160 @@
-USE ChinookDW
+USE ChinookDW;
+GO
 
 -- Only for the first load
-DELETE FROM Fact_Sales;
-DELETE FROM Dim_Product;
-DELETE FROM Dim_Customer;
-DELETE FROM Dim_Employee;
-DELETE FROM Dim_Sales_Info;
-DELETE FROM Dim_Date;
-
--- 0
-INSERT INTO Dim_Sales_Info (Invoice_Line_Id, Invoice_Id)
-SELECT [InvoiceLineId], [InvoiceId]
-FROM [ChinookStaging].[dbo].[Invoice_Line]
-
-INSERT INTO Dim_Sales_Info (Billing_Address, Billing_State, Billing_City, Billing_Country, Billing_Postal_Code)
-SELECT [BillingAddress], [BillingState], [BillingCity], [BillingCountry], [BillingPostalCode]
-FROM [ChinookStaging].[dbo].[Invoice]
+TRUNCATE TABLE dbo.Fact_Sales;
+TRUNCATE TABLE dbo.Dim_Product_Music;
+TRUNCATE TABLE dbo.Dim_Customer;
+TRUNCATE TABLE dbo.Dim_Employee;
+TRUNCATE TABLE dbo.Dim_Sales_Info;
 
 
--- 1
+--Drop constraints and recreate them after
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimDate_DateId_FK;
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimSalesInfo_InvoiceLineId_FK;
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimCustomer_CustomerSupportRepId_FK;
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimCustomer_CustomerId_FK;
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimEmployee_EmployeeId_FK;
+ALTER TABLE dbo.Fact_Sales DROP CONSTRAINT IF EXISTS FactSales_DimProductMusic_TrackId_FK;
+GO
+
+
+-- Load values to Dim_Sales_Info
+INSERT INTO Dim_Sales_Info (
+	Invoice_Line_Id, 
+	Invoice_Id, 
+	Billing_Address, 
+	Billing_State, 
+	Billing_City, 
+	Billing_Country, 
+	Billing_Postal_Code)
+SELECT 
+	b.[InvoiceLineId], 
+	a.[InvoiceId],
+	a.[BillingAddress],
+	a.[BillingState],
+	a.[BillingCity],
+	a.[BillingCountry],
+	a.[BillingPostalCode]
+FROM [ChinookStaging].[dbo].[Invoice] a
+	INNER JOIN [ChinookStaging].[dbo].[Invoice_Line] b ON a.InvoiceId = b.InvoiceId;
+
+
+
+-- Load values to Dim_Employee
 INSERT INTO Dim_Employee (
-	[Employee_Id], [Last_Name], [First_Name], [Title], [Reports_To], [Employee_Address], 
-	[City], [Employee_State], [Country], [Postal_Code], [Phone], [Fax], [Email], [Birth_Date], [Hire_Date])
+	[Employee_Id],
+	[Employee_Last_Name],
+	[Employee_First_Name],
+	[Employee_Title],
+	[Employee_Reports_To],
+	[Employee_Address], 
+	[Employee_City],
+	[Employee_State],
+	[Employee_Country],
+	[Employee_Postal_Code],
+	[Employee_Phone],
+	[Employee_Fax],
+	[Employee_Email],
+	[Employee_Birth_Date],
+	[Employee_Hire_Date])
 SELECT 
-	[EmployeeID], [LastName], [FirstName], [Title], [ReportsTo], [Address], 
-	[City], [State], [Country], [PostalCode], [Phone], [Fax], [Email], [BirthDate], [HireDate]
-FROM [ChinookStaging].[dbo].[Employee]
+	[EmployeeID],
+	[LastName],
+	[FirstName],
+	[Title],
+    CASE WHEN [ReportsTo] IS NULL THEN 0 ELSE [ReportsTo] END,
+	CASE WHEN [Address] IS NULL THEN 'N/A' ELSE [Address] END,
+    CASE WHEN [City] IS NULL THEN 'N/A' ELSE [City] END,
+    CASE WHEN [State] IS NULL THEN 'N/A' ELSE [STATE] END, 
+    CASE WHEN [Country] IS NULL THEN 'N/A' ELSE [Country] END,
+	CASE WHEN [PostalCode] IS NULL THEN 'N/A' ELSE [PostalCode] END,
+	[Phone],
+	[Fax],
+	[Email],
+	[BirthDate],
+	[HireDate]
+FROM [ChinookStaging].[dbo].[Employee];
 
 
---2
+-- Load values to Dim_Customer
 INSERT INTO Dim_Customer(
-	[Customer_Id], [First_Name], [Last_Name], [Company], [Customer_Address], [City], [Customer_State], [Country], 
-	[Postal_Code], [Phone], [Fax], [Email], [Support_Rep_Id])
+	[Customer_Id],
+	[Customer_First_Name],
+	[Customer_Last_Name],
+	[Customer_Company],
+	[Customer_Address],
+	[Customer_City],
+	[Customer_State],
+	[Customer_Country], 
+	[Customer_Postal_Code],
+	[Customer_Phone],
+	[Customer_Fax],
+	[Customer_Email],
+	[Customer_Support_Rep_Id])
 SELECT 
-	[CustomerId], [FirstName], [LastName], [Company], [Address], [City], [State], [Country], 
-	[PostalCode], [Phone], [Fax], [Email], [SupportRepId]
-FROM [ChinookStaging].[dbo].[Customer]
+	[CustomerId],
+	[FirstName],
+	[LastName],
+	CASE WHEN [Company] IS NULL THEN 'N/A' ELSE [Company] END,
+	CASE WHEN [Address] IS NULL THEN 'N/A' ELSE [Address] END,
+	CASE WHEN [City] IS NULL THEN 'N/A' ELSE [City] END,
+	CASE WHEN [State] IS NULL THEN 'N/A' ELSE [STATE] END, 
+    CASE WHEN [Country] IS NULL THEN 'N/A' ELSE [Country] END,
+	CASE WHEN [PostalCode] IS NULL THEN 'N/A' ELSE [PostalCode] END,
+	[Phone],
+	[Fax],
+	[Email],
+	[SupportRepId]
+FROM [ChinookStaging].[dbo].[Customer];
 
 
---3
-INSERT INTO Dim_Product(
-	[Track_Id], [Track_Name], [Album_Id], [Media_Type_Id], [Genre_Id], [Track_Composer], 
-	[Track_Miliseconds],[Track_Bytes], [Unit_Price])
+-- Load values to Dim_Product_Music
+INSERT INTO Dim_Product_Music(
+	[Track_Id],
+	[Track_Name],
+	[Track_Composer],
+	[Track_Miliseconds],
+	[Track_Bytes],
+	[Track_Unit_Price],
+	[Album_Id],
+	[Album_Title],
+	[Artist_Id],
+	[Artist_Name],
+	[Playlist_Id],
+	[Playlist_Name],
+	[Media_Type_Id],
+	[Media_Type_Name],
+	[Genre_Id],
+	[Genre_Name]
 SELECT 
-	[TrackId], [Name], [AlbumId], [MediaTypeId], [GenreId], [Composer], 
-	[Milliseconds], [Bytes], [UnitPrice]
-FROM [ChinookStaging].[dbo].[Track]
+	a.[TrackId], 
+	a.[Name], 
+	a.[Composer],
+	a.[Milliseconds],
+	a.[Bytes],
+	a.[UnitPrice],
+	a.[AlbumId],
+	b.[Title],
+	b.[ArtistId],
+	--c.[Name],
+	d.[PlaylistId],
+	--d.[Name],
+	a.[MediaTypeId],
+	e.[Name],
+	a.[GenreId],
+	f.[Name]
 
-INSERT INTO Dim_Product([Playlist_Id], [Playlist_Name])
-SELECT [PlaylistId], [Name]
-FROM [ChinookStaging].[dbo].[Playlist]
-
-INSERT INTO Dim_Product([Genre_Name])
-SELECT [Name]
-FROM [ChinookStaging].[dbo].[Genre]
-
-INSERT INTO Dim_Product([Media_Type_Name])
-SELECT [Name]
-FROM [ChinookStaging].[dbo].[Media_Type]
-
-INSERT INTO Dim_Product([Album_Title])
-SELECT [Title]
-FROM [ChinookStaging].[dbo].[Album]
-
-INSERT INTO Dim_Product([Artist_Id], [Artist_Name])
-SELECT [ArtistId], [Name]
-FROM [ChinookStaging].[dbo].[Artist]
+FROM [ChinookStaging].[dbo].[Track] a
+	INNER JOIN [ChinookStaging].[dbo].[Album] b ON 
+	INNER JOIN [ChinookStaging].[dbo].[Playlist] ON
+	INNER JOIN [ChinookStaging].[dbo].[Genre] ON
+	INNER JOIN [ChinookStaging].[dbo].[Media_Type] ON
+	INNER JOIN [ChinookStaging].[dbo].[Artist] ON
 
 
---4
+
+-- Load values to Fact_Sales
 INSERT INTO Fact_Sales([Invoice_Id], [Invoice_Line_Id], [Track_Id], [Unit_Price], [Quantity])
 SELECT [InvoiceId], [InvoiceLineId], [TrackId], [UnitPrice], [Quantity]
 FROM [ChinookStaging].[dbo].[Invoice_Line]
@@ -90,3 +176,22 @@ INNER JOIN NorthwindDW.dbo.DimProduct
     ON NorthwindDW.dbo.DimProduct.ProductID=NorthwindStaging.dbo.Sales.ProductID
 
 
+--recreate the constraints Foreign Keys
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimDate_DateId_FK FOREIGN KEY (Date_Id)
+    REFERENCES Dim_Date(Date_Id);
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimSalesInfo_InvoiceLineId_FK FOREIGN KEY (Invoice_Line_Id)
+    REFERENCES Dim_Sales_Info (Invoice_Line_Id);
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimCustomer_CustomerSupportRepId_FK FOREIGN KEY (Employee_Id)
+    REFERENCES Dim_Customer (Customer_Support_Rep_Id);
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimCustomer_CustomerId_FK FOREIGN KEY (Customer_Id)
+    REFERENCES Dim_Customer (Customer_Id);
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimEmployee_EmployeeId_FK FOREIGN KEY (Employee_Id)
+    REFERENCES Dim_Employee (Employee_Id);
+
+ALTER TABLE Fact_Sales ADD CONSTRAINT FactSales_DimProductMusic_TrackId_FK FOREIGN KEY (Track_Id)
+    REFERENCES Dim_Product_Music (Track_Id);
