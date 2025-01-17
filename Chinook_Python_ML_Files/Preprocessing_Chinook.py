0969adc99ee4a73ba91f4683b577a9b498f1844d
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-17 23:47:58 +0200

    Create Preprocessing_Chinook.py

---
+++ Chinook_Python_ML_Files/Preprocessing_Chinook.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+import seaborn
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+print("All libraries imported successfully!fdfv")
