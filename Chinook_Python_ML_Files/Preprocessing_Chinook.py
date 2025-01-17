1707370adf02bfe26fa9bfd1cdde632e880c0d6b
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-18 00:55:10 +0200

    ren

--- Chinook_Python_ML_Files/Preprocessing_Chinook.py
+++ Chinook_Python_ML_Files/Preprocessing_Chinook.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-import seaborn
+import seaborn as snb
 from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -9,9 +9,11 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-print("All libraries imported successfulhnrtdly!")
+print("All libraries myimported successfulhnrtdly!")
 
 df = pd.read_csv('Query_Join_Tables_Chinook.csv')
 
+
 df.fillna(0, inplace=True)
-print("f16")
+print("f1ntrb vb vb  hgmghmgd6")
+print("f1ntrb vb vb  hgmghmgd6")
