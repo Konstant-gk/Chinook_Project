7caaabfe3f5f558faf87d871085ce89931e93903
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-22 01:22:19 +0200

    Create ChinookDW_Run_All_Tables.sql

---
+++ Chinook_Data_Warehouse_Files/Chinook_DW_Solution/SQL Server Scripts/ChinookDW_Run_All_Tables.sql
@@ -0,0 +1,9 @@
+USE ChinookDW
+GO
+
+SELECT * FROM [dbo].[Dim_Customer]
+SELECT * FROM [dbo].[Dim_Date]
+SELECT * FROM [dbo].[Dim_Employee]
+SELECT * FROM [dbo].[Dim_Sales_Info]
+SELECT * FROM [dbo].[Dim_Product_Music]
+SELECT * FROM [dbo].[Fact_Sales]
