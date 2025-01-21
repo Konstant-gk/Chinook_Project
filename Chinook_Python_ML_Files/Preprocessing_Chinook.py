d356a5079712b2e7e5b9f4e58e5a47c38ceffa9c
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-21 14:50:14 +0200

    Update Preprocessing_Chinook.py

--- Chinook_Python_ML_Files/Preprocessing_Chinook.py
+++ Chinook_Python_ML_Files/Preprocessing_Chinook.py
@@ -1,15 +1,47 @@
-import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import seaborn as snb
-from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
-print("All libraries imported successfully!")
+df = pd.read_csv('Rows_Query_Join_Tables_Chinook.csv')
 
-df = pd.read_csv('Query_Join_Tables_Chinook.csv')
-df.fillna(0, inplace=True)
+print(df)
+
+print(df['Employee_Age'].value_counts())
+
+print(df['Employee_Role'].value_counts())
+
+print(df['TotalRevenue'].describe())
+
+plt.hist(df['Employee_Age'], bins=20)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
