ba14f58db16691f56f7b1549155703c8dc985f0c
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-21 14:50:23 +0200

    Create Rows_SQL_Join_Tables_Chinook.sql

---
+++ Chinook_Python_ML_Files/Rows_SQL_Join_Tables_Chinook.sql
@@ -0,0 +1,43 @@
+USE Chinook
+GO
+
+SELECT
+    a.[EmployeeId],
+	a.[FirstName] AS Employee_FirstName,
+    a.[LastName] AS Employee_LastName,
+    a.[HireDate] AS Employee_HireDate,
+	a.[BirthDate] AS Employee_BirthDate,
+	DATEDIFF(YEAR, a.[BirthDate], GETDATE()) -
+        CASE 
+            WHEN DATEADD(YEAR, DATEDIFF(YEAR, a.[BirthDate], GETDATE()), a.[BirthDate]) > GETDATE() 
+            THEN 1 
+            ELSE 0 
+        END AS Employee_Age,
+	a.[ReportsTo],
+	a.[Title] AS Employee_Role,
+	a.[Country] AS Employee_Country,
+	b.[CustomerId],
+	b.[FirstName] + ' ' + b.[LastName] AS Customer_FullName,
+	d.[UnitPrice],
+	c.[Total],
+    COUNT(c.[InvoiceId]) AS TotalInvoices,
+    SUM(c.[Total]) AS TotalRevenue,
+    AVG(c.[Total]) AS AvgRevenue
+FROM [Chinook].[dbo].[Employee] a
+LEFT JOIN [Chinook].[dbo].[Customer] b ON a.[EmployeeId] = b.[SupportRepId]
+LEFT JOIN [Chinook].[dbo].[Invoice] c ON b.[CustomerId] = c.[CustomerId]
+LEFT JOIN [Chinook].[dbo].[InvoiceLine] d ON d.[InvoiceId] = c.[InvoiceId]
+GROUP BY 
+	a.[EmployeeId],
+	a.[FirstName],
+	a.[LastName],
+	a.[HireDate],
+	a.[BirthDate],
+	a.[ReportsTo],
+	a.[Title],
+	a.[Country],
+	b.[CustomerId],
+	b.[FirstName] + ' ' + b.[LastName],
+	d.[UnitPrice],
+	c.[Total]
+ORDER BY [EmployeeId] ASC;
