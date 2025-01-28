ffe01914e7ef9c1f4791e19425b5b72539de5ea9
Konstantinos Gkaravelos <konstant.gka@gmail.com>
2025-01-28 15:44:24 +0200

    Delete ChinookDW_Run_All_Tables.sql

--- Chinook_Data_Warehouse_Files/Chinook_DW_Solution/SQL Server Scripts/ChinookDW_Run_All_Tables.sql
+++
@@ -1,9 +0,0 @@
-USE ChinookDW
-GO
-
-SELECT * FROM [dbo].[Dim_Customer]
-SELECT * FROM [dbo].[Dim_Date]
-SELECT * FROM [dbo].[Dim_Employee]
-SELECT * FROM [dbo].[Dim_Sales_Info]
-SELECT * FROM [dbo].[Dim_Product_Music]
-SELECT * FROM [dbo].[Fact_Sales]
